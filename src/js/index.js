be867df02e1b56d7a0b534cb78a0a6c249e23258
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2023-09-20 20:14:48 +0900

    feat: geomtry, material, renderer, scene, camera

--- src/js/index.js
+++ src/js/index.js
@@ -1,114 +1,75 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import printIsland from '../mesh/island.js';
-import printTangerine from '../mesh/tangerine.js';
-import printTree from '../mesh/tree.js';
-import printMountain from '../mesh/mountain.js';
-import printStone from '../mesh/stone.js'
+import * as THREE from "three";
 
-// 장면구조
+// choose result canvas
+
+const $result = document.getElementById("result");
+
+// 1. Scene ㅁ화ㅇㄴㅇ서 보줄 객ㅔㄹㅡㄹㄴ담는
+
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x7ccad5)
-
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 10, 20);
+scene.background = new THREE.Color(0xffe287);
+//2. Camera : perspective that sees the scene.
+const camera = new THREE.PerspectiveCamera(
+  50, // 야시야각 the bigger number is, the wider view is
+  $result.clientWidth / $result.clientHeight,
+  0.1, // min distance
+  1000 // max distance
+);
+camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-renderer.shadowMap.enabled = true
+//3. Renderer : render the scene
+const renderer = new THREE.WebGLRenderer({
+  canvas: $result,
+  antialias: true, // to prevent stair effects
+});
+renderer.setSize($result.clientWidth, $result.clientHeight); // renderer itself's size beecame same as canvas size.
 
-// 도형
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshStandardMaterial({
-//   color: 0xffe272,
-// })
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
+// add renderer to the body
+// document.body.appendChild(renderer.domElement);
 
-const island = printIsland();
-island.position.y = -1.5;
-scene.add(island);
+// and now render the renderer
+renderer.render(scene, camera);
 
-const tangerine = printTangerine();
-tangerine.position.set(-5, 0, -1);
-scene.add(tangerine);
-const miniTan = printTangerine();
-miniTan.scale.set(0.7, 0.7, 0.7);
-miniTan.position.set(-6, 0, 1.5);
-scene.add(miniTan);
-console.log(miniTan);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(light);
 
-const tree = printTree();
-tree.position.set(5, -0.5, -1);
-tree.rotation.y = Math.PI / -3;
-scene.add(tree);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x2e6ff2 });
 
-const miniTree = printTree();
-miniTree.position.set(6.5, -1, 1);
-miniTree.scale.set(0.6, 0.6, 0.6);
-scene.add(miniTree);
+// Mesh : 3 dimensional object
+// geometry + material = mesh
+// geometry : shape of the object
+// material : color/fabric of the object
+const geo1 = new THREE.BoxGeometry(1, 1, 1); // 1* 1* 1 box
+const geo2 = new THREE.ConeGeometry(1, 1, 32); // radius, height, segment
+const geo3 = new THREE.SphereGeometry(1, 32, 32); // radius, width segment, height segment
+const geo4 = new THREE.PlaneGeometry(1, 1, 32, 32); // width, height, width segment, height segment 2 dimensions
+const geo5 = new THREE.TorusGeometry(1, 0.35, 32, 100); // donut, radius, tube, radial segment, tubular segment
 
-const mountain = printMountain();
-mountain.scale.set(1.2, 1.6, 1);
-mountain.position.set(0, 1, -1.8);
-scene.add(mountain);
+const box = new THREE.Mesh(geometry, material);
 
-const myChar = printStone();
-myChar.position.set(3, -0.5, 1);
-myChar.scale.set(0.9, 0.9, 0.9)
-myChar.rotation.y = Math.PI / -8
-scene.add(myChar);
+// will be added to scene to make this box shown.
 
-const modelLoader = new GLTFLoader();
-modelLoader.load('../../src/models/Lycat-3d.glb', (gltf) => {
-  const model = gltf.scene
-  model.position.set(-2.8, -1.3, 1);
-  model.rotation.y = Math.PI / 8
-  scene.add(model);
-  // console.log(model);
-  for (const mesh of model.children) {
-    mesh.castShadow = true;
-  }
-})
+scene.add(box);
 
+// axesHelper : x,y,z axis
 
-// 빛
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
-const directionLight = new THREE.DirectionalLight(0xffffff, 1);
-directionLight.position.set(-10, 10, 10);
-scene.add(directionLight);
-directionLight.castShadow = true
-
-const pl1 = new THREE.PointLight(0xff8c00, 1.5);
-pl1.position.set(5, 0, 0);
-scene.add(pl1);
-
-const pl2 = new THREE.PointLight(0xffe287, 2);
-pl2.position.set(-3, 2, 0);
-scene.add(pl2);
-
-// OrbitControls
-const control = new OrbitControls(camera, renderer.domElement);
-control.autoRotate = true;
-control.autoRotateSpeed = -1;
-control.minDistance = 10;
-control.maxDistance = 30;
-
-
+// function to use in requestAnimationFrame
 function animate() {
-  control.update();
-  renderer.render(scene, camera);
+  // whenever animate is called, it will rotate the box.
+  box.rotation.y += 0.01;
   requestAnimationFrame(animate);
+  // even though y is being modified, the changes not reflected on the screen.
+  renderer.render(scene, camera);
 }
+// before rendering next frame, it repeatedly call the animate.
 animate();
 
-window.addEventListener('resize', () => {
+// for the resizing
+window.addEventListener("resize", () => {
+  // update the camera
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
